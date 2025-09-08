ece0485b98222f466ade53ef3eee908aec5f4f8d
khassamx <khasamxx@gmail.com>
2025-09-08 13:09:17 -0300

    Crear consola2/src/utils/antiArabic.js

---
+++ consola2/src/utils/antiArabic.js
@@ -0,0 +1,23 @@
+const { log } = require('../../../src/utils/logger.js');
+const { ARABIC_PREFIXES } = require('../config/settings.js');
+
+const checkAntiArabic = async (sock, groupJid, participantJid) => {
+    const countryCode = participantJid.split('@')[0].substring(0, 3);
+    const isArabic = ARABIC_PREFIXES.includes(countryCode);
+
+    if (isArabic) {
+        try {
+            await sock.groupParticipantsUpdate(groupJid, [participantJid], 'remove');
+            log(`🚫 Anti-Árabe: Usuario con código '${countryCode}' expulsado de [${groupJid}].`);
+            return true;
+        } catch (e) {
+            log(`Error en Anti-Árabe al expulsar: ${e.message}`);
+            return false;
+        }
+    }
+    return false;
+};
+
+module.exports = {
+    checkAntiArabic
+};
