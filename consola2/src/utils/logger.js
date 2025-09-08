668611e1a034c8aad839fbc615c3d451f805470a
khassamx <khasamxx@gmail.com>
2025-09-08 13:28:50 -0300

    Actualizar logger.js

--- consola2/src/utils/logger.js
+++ consola2/src/utils/logger.js
@@ -1,14 +1,49 @@
-const log = (message) => {
-    const timestamp = new Date().toLocaleString();
-    console.log(`[${timestamp}] INFO: ${message}`);
-};
+const chalk = require("chalk");
+const dayjs = require("dayjs");
+const fs = require("fs");
+const path = require("path");
 
-const logError = (message) => {
-    const timestamp = new Date().toLocaleString();
-    console.error(`[${timestamp}] ERROR: ${message}`);
-};
+const logFile = path.join(__dirname, "../../../logs.txt");
 
+// Función para obtener timestamp
+function timestamp() {
+  return dayjs().format("YYYY-MM-DD HH:mm:ss");
+}
+
+// Función para escribir en el archivo de logs
+function writeToFile(message) {
+  fs.appendFile(logFile, message + "\n", (err) => {
+    if (err) {
+      console.error(chalk.red("Error al escribir en el archivo de logs:"), err);
+    }
+  });
+}
+
+// Logger principal
 module.exports = {
-    log,
-    logError
+  info: (...msg) => {
+    const logMessage = `[INFO ${timestamp()}] ${msg.join(" ")}`;
+    console.log(chalk.blue(logMessage));
+    writeToFile(logMessage);
+  },
+  warn: (...msg) => {
+    const logMessage = `[WARN ${timestamp()}] ${msg.join(" ")}`;
+    console.log(chalk.yellow(logMessage));
+    writeToFile(logMessage);
+  },
+  error: (...msg) => {
+    const logMessage = `[ERROR ${timestamp()}] ${msg.join(" ")}`;
+    console.error(chalk.red(logMessage));
+    writeToFile(logMessage);
+  },
+  success: (...msg) => {
+    const logMessage = `[SUCCESS ${timestamp()}] ${msg.join(" ")}`;
+    console.log(chalk.green(logMessage));
+    writeToFile(logMessage);
+  },
+  log: (...msg) => {
+    const logMessage = `[LOG ${timestamp()}] ${msg.join(" ")}`;
+    console.log(chalk.white(logMessage));
+    writeToFile(logMessage);
+  },
 };
